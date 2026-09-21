63a042ea41fe9b2a43e8f74565b3f0d7426f29cb
NX1X <124435875+NX1X@users.noreply.github.com>
2026-09-21 17:02:05 +0300

    release: v0.2.1 (#34)

--- src/pfsentinel/__init__.py
+++ src/pfsentinel/__init__.py
@@ -1,6 +1,6 @@
 """pfSentinel - pfSense Backup Solution for Homelabs."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 DOCS_URL = "https://pfs.nx1xlab.dev/docs"
 ISSUES_URL = "https://github.com/NX1X/pfSentinel/issues"
